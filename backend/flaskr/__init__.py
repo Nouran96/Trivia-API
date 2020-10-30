de14093eb0135a4108b374df6425b61c75f5af40
Nouran Samy <nourans.96@yahoo.com>
2020-10-30 19:03:31 +0200

    DELETE Endpoint for questions

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -79,7 +79,8 @@ def create_app(test_config=None):
       "questions": paginate_questions(request, questions),
       "total_questions": len(questions),
       "categories": {category.id: category.type for category in categories},
-      "current_category": categories[0].type
+      "current_category": categories[0].type,
+      "success": True
     })
 
   '''
@@ -89,6 +90,20 @@ def create_app(test_config=None):
   TEST: When you click the trash icon next to a question, the question will be removed.
   This removal will persist in the database and when you refresh the page. 
   '''
+  @app.route('/questions/<int:question_id>', methods=['DELETE'])
+  @cross_origin()
+  def delete_question(question_id):
+    question = Question.query.filter(Question.id == question_id).one_or_none()
+
+    if question is None:
+      abort(404)
+
+    question.delete()
+
+    return jsonify({
+      "success": True,
+      "deleted": question_id
+    })
 
   '''
   @TODO: 
@@ -139,6 +154,13 @@ def create_app(test_config=None):
   Create error handlers for all expected errors 
   including 404 and 422. 
   '''
+  @app.errorhandler(404)
+  def not_found():
+    return jsonify({
+      "error": 404,
+      "success": False,
+      "message": "Resource Not Found"
+    }), 404
   
   return app
 
